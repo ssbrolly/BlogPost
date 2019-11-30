9b5d4366c6cc394f9d6c5f5fb52988306cd63a07
Usong Cho <silvermouse4012@gmail.com>
2019-11-29 20:02:33 -0800

    initial commit

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import classes from './Blog.module.css'
+import Post from '../components/Post/Post';
+import NewPost from '../components/NewPost/NewPost';
+import FullPost from '../components/FullPost/FullPost';
 
 
 class Blog extends Component {
     render() {
         return (
             <div>
-                <section className={classes.Posts}>
-                    <p>something, something</p>
+                <section className={classes.Blog}>
+                    <Post />
+                    <Post />
+                    <Post />
                 </section>
                 <section>
-                    <p>somthing, something</p>
+                    <FullPost />
                 </section>
                 <section>
-                    <p>something, something</p>
+                    <NewPost />
                 </section>
             </div>
         );
