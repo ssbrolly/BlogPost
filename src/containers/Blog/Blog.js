b10db128ef5056a71f2214b5be8376b8a163b325
Usong Cho <silvermouse4012@gmail.com>
2019-12-17 20:34:42 -0800

    react-router Link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import classes from './Blog.module.css'
 import Posts from '../Blog/Posts/Posts';
+import { Route, Link } from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -9,12 +11,18 @@ class Blog extends Component {
                 <header className={classes.Blog}>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">Home Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={() => (<Posts />)} /> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div >
         );
     };
