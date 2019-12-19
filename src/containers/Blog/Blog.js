ca5f57fc4b0db73b752e33e581bf1a85eb8ec782
Usong Cho <silvermouse4012@gmail.com>
2019-12-19 14:25:16 -0800

    added NavLink

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,34 +1,73 @@
 import React, { Component } from 'react';
-import classes from './Blog.module.css'
+import './Blog.css'
 import Posts from '../Blog/Posts/Posts';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render() {
         return (
-            <div>
-                <header className={classes.Blog}>
+            <div className="Blog">
+                <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline',
+                                }}
+                            >Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => (<Posts />)} /> */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div >
         );
     };
 };
 
 export default Blog;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
