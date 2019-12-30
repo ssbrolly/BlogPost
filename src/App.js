b2d45f7c586e553bb3b8fd57c9734c3e9b2edd64
Usong Cho <silvermouse4012@gmail.com>
2019-12-30 15:47:28 -0800

    refactored for optimization

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
     render() {
         return (
+            // <BrowserRouter basename="/">
             <BrowserRouter>
-                <div>
+                <div className="App">
                     <Blog />
                 </div>
             </BrowserRouter>
